02914cdad45f781366f9c52b019bf3e779a15c46
ZhaoFengf <275947878@qq.com>
2026-07-17 10:32:40 +0800

    实现水晶攻击部分技能与技能树的绑定工作

--- Assets/Scripts/Skill/Crystal_Skill.cs
+++ Assets/Scripts/Skill/Crystal_Skill.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //似乎有点bug，就是水晶不能自动追踪时候会直接炸开，这个后续得修改一下
 public class Crystal_Skill : Skill
@@ -9,22 +10,81 @@ public class Crystal_Skill : Skill
     [SerializeField] private float crystalDuration = 2.5f;
     private GameObject currentCrystal;
 
+    [Header("Crystal simple")]
+    [SerializeField] private UISkillTreeSlot unlockCrystalButton;
+    public bool crystalUnlocked { get; private set; }
+
     [Header("Crystal mirage")]
+    [SerializeField] private UISkillTreeSlot unlockCloneInsteadButton;
     [SerializeField] private bool cloneInsteadOfCrystal;
 
     [Header("Explosive crystal")]
+    [SerializeField] private UISkillTreeSlot unlockExplosiveButton;
     [SerializeField] private bool canExplode;
 
     [Header("Moving crystal")]
+    [SerializeField] private UISkillTreeSlot unlockMovingCrystalButton;
     [SerializeField] private bool canMoveToEnemy;
     [SerializeField] private float moveSpeed;
 
     [Header("Multi stacking crystal")]
+    [SerializeField] private UISkillTreeSlot unlockMultiCrystalButton;
     [SerializeField] private bool canUseMultiStack;
     [SerializeField] private int amountOfStacks;
     [SerializeField] private float multiStackCoolDown;
     [SerializeField] private float useTimeWindow;
     [SerializeField] private List<GameObject> crystalleft = new List<GameObject>();
+
+    protected override void Start()
+    {
+        base.Start();
+
+        unlockCrystalButton.GetComponent<Button>().onClick.AddListener(UnlockCrystal);
+
+        unlockCloneInsteadButton.GetComponent<Button>().onClick.AddListener(UnlockCrystalMirage);
+
+        unlockExplosiveButton.GetComponent<Button>().onClick.AddListener(UnlockExplosiveCrystal);
+
+        unlockMovingCrystalButton.GetComponent<Button>().onClick.AddListener(UnlockMovingCrystal);
+
+        unlockMultiCrystalButton.GetComponent<Button>().onClick.AddListener(UnlockMultiSatckCrystal);
+    }
+
+    #region Unlock skill
+
+    private void UnlockCrystal()
+    {
+        if (unlockCrystalButton.unlocked)
+            crystalUnlocked = true;
+    }
+
+    private void UnlockCrystalMirage()
+    {
+        if (unlockCloneInsteadButton.unlocked)
+            cloneInsteadOfCrystal = true;
+    }
+    
+    private void UnlockExplosiveCrystal()
+    {
+        if (unlockExplosiveButton.unlocked)
+            canExplode = true;
+    }
+    
+    private void UnlockMovingCrystal()
+    {
+        if (unlockMovingCrystalButton.unlocked)
+            canMoveToEnemy = true;
+    }
+    
+    private void UnlockMultiSatckCrystal()
+    {
+        if (unlockMultiCrystalButton.unlocked)
+            canUseMultiStack = true;
+    }
+    #endregion
+
+
+
 
     public override void UseSkill()
     {
@@ -41,8 +101,8 @@ public class Crystal_Skill : Skill
         else
         {
             //这里我们换成可以在一定范围内移动到敌人身上，否则可以交换位置。但是移动后原地爆炸的还没解决
-            //if (canMoveToEnemy)
-            //    return;
+            if (canMoveToEnemy)
+                return;
 
             Vector2 playerPos = player.transform.position;
             player.transform.position = currentCrystal.transform.position;
@@ -71,8 +131,7 @@ public class Crystal_Skill : Skill
         //CurentCrystalChooseRandomTarget();
     }
 
-    public void CurentCrystalChooseRandomTarget() =>
-        currentCrystal.GetComponent<Crystal_Skill_Controller>()?.ChooseRandomEnemy();
+    public void CurentCrystalChooseRandomTarget() =>  currentCrystal.GetComponent<Crystal_Skill_Controller>()?.ChooseRandomEnemy();
 
     private bool canUseMultiCrystal()
     {
@@ -93,8 +152,6 @@ public class Crystal_Skill : Skill
             
                 if(crystalleft.Count <= 0)
                 {
-                    //colldown
-                    //Debug.Log("Refilling crystal");
                     cooldown = multiStackCoolDown;
                     RefilCrystal();
                 }
